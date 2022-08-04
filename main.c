99ab1698a5129271aee2f5c0fc708e2cad94a2db
Amallamzaouk <109852815+Amallamzaouk@users.noreply.github.com>
2022-08-04 15:40:35 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -3,6 +3,36 @@
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+
+    int choix;
+    float prix;
+    char *nomechoix;
+
+    printf("Bienvenu!\n");
+    printf("veuiller choisir un boisson \n");
+    printf("taperez 1 cafe\n taperz 2 the\n taperz 3 chocolat\n taperz 4 soda\n");
+    scanf("%d",&choix);
+
+    switch(choix){
+    case 1:
+            nomechoix = "cafe" ;
+            prix= 2 ;
+            break;
+    case 2:
+            nomechoix = "the" ;
+            prix= 2 ;
+            break;
+
+     case 3:
+            nomechoix = "chocolat" ;
+            prix= 2.5 ;
+            break;
+
+   case 4:
+            nomechoix = "soda" ;
+            prix= 3 ;
+             break;
+             }
+          printf  ("Bienvenu!\n");
 }
+
